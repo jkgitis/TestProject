f9f4fe37d63ee24af62f446604d43ca404b72187
jkgitis <gayatridudu2@gmail.com>
2025-06-18 20:12:20 +0530

    SplashScreen Added

---
+++ app/src/main/java/com/example/testproject/SplashScreen.kt
@@ -0,0 +1,46 @@
+package com.example.clotapp.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.testproject.R
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+//            .clip(RoundedCornerShape(32.dp))
+            .background(Color(0xFF936EF6)), // Purple background
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "App Logo",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .width(160.dp)
+                .height(160.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
